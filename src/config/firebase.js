f35016211fbd92de203d67ee46afea2ffc5430e4
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2024-01-09 18:11:36 +0530

    Added Changes

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAYsXkoNVrhyfc-tYCK9nklXNs1avok7Og",
-  authDomain: "learning-firebase-ec71c.firebaseapp.com",
-  projectId: "learning-firebase-ec71c",
-  storageBucket: "learning-firebase-ec71c.appspot.com",
-  messagingSenderId: "454890573421",
-  appId: "1:454890573421:web:bb90d54252d57b086db964",
-  measurementId: "G-D1RC2BFVVM",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
