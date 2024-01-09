9c189e089a494a65a2f61a391a2a2d051e2af095
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2024-01-09 17:28:02 +0530

    Added auth import to firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAYsXkoNVrhyfc-tYCK9nklXNs1avok7Og",
@@ -11,3 +12,4 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
