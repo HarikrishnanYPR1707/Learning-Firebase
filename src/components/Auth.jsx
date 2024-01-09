08853fdee269f62e2d8d388a343fefbf38b4c4da
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2024-01-09 18:12:28 +0530

    done with signin with email and password

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import { useState } from "react";
+import { auth } from "../config/firebase";
+import { createUserWithEmailAndPassword, signOut } from "firebase/auth";
 
 const Auth = () => {
-  return <div>Auth</div>;
+  const [emailValue, setEmailValue] = useState("");
+  const [passwordValue, setPasswordValue] = useState("");
+
+  //   console.log(auth?.currentUser?.email);
+
+  const handleSignIn = async () => {
+    try {
+      await createUserWithEmailAndPassword(auth, emailValue, passwordValue);
+      setUser(true);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Email..."
+        onChange={(e) => setEmailValue(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password..."
+        onChange={(e) => setPasswordValue(e.target.value)}
+      />
+      <button onClick={handleSignIn}>Sign in</button>
+    </div>
+  );
 };
 
 export default Auth;
