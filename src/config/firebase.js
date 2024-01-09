5c029f1fbec3ba5c53714a33f709e3830b6a4e10
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2024-01-09 17:24:09 +0530

    added firebase.js

---
+++ src/config/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAYsXkoNVrhyfc-tYCK9nklXNs1avok7Og",
+  authDomain: "learning-firebase-ec71c.firebaseapp.com",
+  projectId: "learning-firebase-ec71c",
+  storageBucket: "learning-firebase-ec71c.appspot.com",
+  messagingSenderId: "454890573421",
+  appId: "1:454890573421:web:bb90d54252d57b086db964",
+  measurementId: "G-D1RC2BFVVM",
+};
+
+const app = initializeApp(firebaseConfig);
