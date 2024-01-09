e842f9444e7ae6ea7337b3d4ac69de522c296e0f
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2024-01-09 18:20:30 +0530

    done with logout feature

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -3,15 +3,24 @@ import { auth } from "../config/firebase";
 import { createUserWithEmailAndPassword, signOut } from "firebase/auth";
 
 const Auth = () => {
+  const [user, setUser] = useState(false);
   const [emailValue, setEmailValue] = useState("");
   const [passwordValue, setPasswordValue] = useState("");
-
-  //   console.log(auth?.currentUser?.email);
 
   const handleSignIn = async () => {
     try {
       await createUserWithEmailAndPassword(auth, emailValue, passwordValue);
       setUser(true);
+      console.log(auth?.currentUser?.email);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const logout = async () => {
+    try {
+      await signOut(auth);
+      setUser(false);
     } catch (err) {
       console.error(err);
     }
@@ -30,6 +39,11 @@ const Auth = () => {
         onChange={(e) => setPasswordValue(e.target.value)}
       />
       <button onClick={handleSignIn}>Sign in</button>
+      {user ? (
+        <button style={{ backgroundColor: "red" }} onClick={logout}>
+          logout
+        </button>
+      ) : null}
     </div>
   );
 };
